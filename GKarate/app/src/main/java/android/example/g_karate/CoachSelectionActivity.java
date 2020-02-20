1271d0c0b4533ecf7ac367d289e04da70eb05bb7
Borcherd van Brakell <11700472@student.pxl.be>
2020-02-20 14:15:17 +0100

    creating intents

--- GKarate/app/src/main/java/android/example/g_karate/CoachSelectionActivity.java
+++ GKarate/app/src/main/java/android/example/g_karate/CoachSelectionActivity.java
@@ -1,4 +1,27 @@
 package android.example.g_karate;
 
-public class CoachSelectionActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CoachSelectionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coach_selection);
+
+
+    }
+
+    public void goToExerciseSelection(View view){
+        Intent intent = new Intent(this, ExerciseSelectionActivity.class);
+        startActivity(intent);
+    }
+    public void goToNewExercise(View view){
+        Intent intent = new Intent(this, NewExercise1Activity.class);
+        startActivity(intent);
+    }
+
 }
